afe611cca89e7466aa6783481ca82ab31913b343
egorov anton <yegorov.ant@gmail.com>
2017-02-01 20:17:02 +0400

    cleaning windows mess up

---
+++ broadcastreceiversample/app/src/main/java/ru/ifmo/android/broadcastreceiversample/Constants.java
@@ -0,0 +1,14 @@
+package ru.ifmo.android.broadcastreceiversample;
+
+/**
+ * Created by Anton on 19.01.2017.
+ */
+public class Constants {
+    public static final String KEY_URL = "KEY_URL";
+    public static final String KEY_SUCCESS = "KEY_SUCCESS";
+    public static final String KEY_PICTURE_PATH = "KEY_PICTURE_PATH";
+    public static final String KEY_RESULT_RECEIVER= "KEY_RESULT_RECEIVER";
+
+
+    public static final String IMAGE_SOURCE_URL = "http://breedinfo.ru/uploads/posts/2015-12/5_pug.jpg";
+}
